9216d04d1b0c22ae41d99c128fcdf69ddcd45ea6
Dragus Patrick <dragus.patrick@icloud.com>
2020-02-26 23:00:03 +0200

    back-end for the contact page

--- contact/models.py
+++ contact/models.py
@@ -1,20 +1,44 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel
+)
+from wagtail.core.fields import RichTextField
+from wagtail.contrib.forms.models import (
+    AbstractEmailForm,
+    AbstractFormField
+)
 
 
 # Create your models here.
-class ContactPage(Page):
-    """Contact page class"""
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'ContactPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields',
+    )
+
+
+class ContactPage(AbstractEmailForm):
     templates = "contact/contact_page.html"
 
-    subtitle = models.CharField(max_length=200, null=True, blank=True)
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel("subtitle"),
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel("intro"),
+        InlinePanel("form_fields", label="Form Fields"),
+        FieldPanel("thank_you_text"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel("from_address", classname="col-6"),
+                FieldPanel("to_address", classname="col-6"),
+            ]),
+            FieldPanel("subject"),
+        ], heading="Email Settings"),
     ]
-
-    class Meta:
-        verbose_name = "Contact Page"
-        verbose_name_plural = "Contact Pages"
